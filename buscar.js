948216861e0ea8bc17dc4661e115b76c44fb8255
thurkeppe <130366236+thurkeppe@users.noreply.github.com>
2023-11-15 20:46:35 -0300

    Delete buscar.js

--- buscar.js
+++
@@ -1,7 +0,0 @@
-
-        function realizarBusca() {
-            var termoBusca = document.getElementById("txtBusca").value;
-            alert("Realizar busca com termo: " + termoBusca);
-            // Adicione aqui a lógica para processar a busca
-        }
-    
