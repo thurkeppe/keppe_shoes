5fce255c58e6700c5a8ca3250c48f457b6aae2a5
thurkeppe <130366236+thurkeppe@users.noreply.github.com>
2023-11-29 19:25:26 -0300

    trabalho semi completo

---
+++ keppe shoes 3/java/buscar.js
@@ -0,0 +1,6 @@
+
+        function realizarBusca() {
+            var termoBusca = document.getElementById("txtBusca").value;
+            alert("Realizar busca com termo: " + termoBusca);
+            // Adicione aqui a lógica para processar a busca
+        }
---
+++ keppe shoes 3/java/voltar.js
@@ -0,0 +1,5 @@
+
+        function voltar() {
+            window.history.back();
+        }
+
